3ed05f251a50d83cfbf918120d2a6161b648c0cd
bhlee1020 <100792260+bhlee1020@users.noreply.github.com>
2022-09-14 02:07:49 +0100

    Update new_vp_exec_script.sh

--- VideoProcessor/new_vp_exec_script.sh
+++ VideoProcessor/new_vp_exec_script.sh
@@ -6,5 +6,5 @@ POSTINIT_DF_PATH=/global/scratch/users/bhlee1020/RNAi/Helen/AC/AC_PostInitializa
 # Change this to the name of your conda environment
 source activate project_env
 
-python3 /global/scratch/users/bhlee1020/CassiopeaProcessor_20210804/VideoProcessor_v2.py $1 $POSTINIT_DF_PATH
+python3 /global/scratch/users/bhlee1020/VideoProcessor/VideoProcessor_v2.py $1 $POSTINIT_DF_PATH
 
